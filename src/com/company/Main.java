0a4eca9b9d8675874efed90ab7927aa6551183fc
sune <sune.bro@ka-net.dk>
2019-11-09 21:59:53 +0100

    Added JDBC

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+import java.util.Scanner;
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Wellcome to the Minecraftdb");
+       Connection connection = null;
+       connection = DriverManager.getConnection
+               ("jdbc:mysql://localhost:3306/Minecraft?serverTimezone=UTC",
+                       "user",
+                       "1970");
+        System.out.println("Connoction ok");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which Creature would you like to know about?");
+        String CreatureName = scanner.nextLine();
+
+    }
+
+}
