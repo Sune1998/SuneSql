9bc045e4fa79bf543b1566cc8ae8984b053d2d27
sune <sune.bro@ka-net.dk>
2019-11-09 22:51:23 +0100

    Added stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,45 @@
 package com.company;
-import java.util.Scanner;
 import java.sql.*;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         System.out.println("Wellcome to the Minecraftdb");
+
        Connection connection = null;
        connection = DriverManager.getConnection
                ("jdbc:mysql://localhost:3306/Minecraft?serverTimezone=UTC",
                        "user",
                        "1970");
+
         System.out.println("Connoction ok");
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which Creature would you like to know about?");
+        System.out.println("Which Creature would you like to know about? ±");
         String creatureName = scanner.nextLine();
-        String newStatement = "SELECT * FROM Minecraft.Cretures WHERE Name LIKE ' " + creatureName +"';";
+        String newStatement = "SELECT * FROM Minecraft.Cretures WHERE Name LIKE '" + creatureName +"';";
 
         Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery
+                (newStatement);
+        System.out.println("The flowing statement has been executed" + newStatement);
+        System.out.println("results returned" + "\n");
+
+
+        int i = 0;
+        while (resultSet.next()){
+            i++;
+        System.out.println(resultSet.getString(1) + "" +
+                 resultSet.getString(2) + "," +
+                 resultSet.getString(3) + "," +
+                "Name: " +
+                 resultSet.getString("Name"));}
+
+        connection.close();
+        System.out.println();
 
 
 
